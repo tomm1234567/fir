887c6ae0026df11e01fbd4a3daddf8fc58b12cc0
tomm1234567 <rjrrheg@gmail.con>
2025-04-26 12:24:11 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
-    host: '0.0.0.0',
+    host:'0.0.0.0',
     port: process.env.PORT || 5173,
-    allowedHosts: ['renderload.onrender.com']
+    allowedHosts: ['all'],
   }
 })
